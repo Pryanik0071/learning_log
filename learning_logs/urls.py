f596eccf932762fa5141e7f4dc9a74f4fc46bdb6
Dmitry <di.mesler.2010@yandex.ru>
2022-08-19 21:42:25 +0300

    Add: topics + detail topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,9 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Страница со списком всех тем
+    path('topics/', views.topics, name='topics'),
+    # Страница с подробной информацией по отдельной теме
+    path('toics/<int:topic_id>/', views.topic, name='topic'),
 ]
