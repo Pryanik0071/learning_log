5b12e3b59ede4e4c608a8d0ed1fac5c0453c9f72
Dmitry <di.mesler.2010@yandex.ru>
2022-08-23 04:11:50 +0300

    Add: User registration

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Авторизация по умолчанию
     path('', include('django.contrib.auth.urls')),
+    # Страница регистрации
+    path('register/', views.register, name='register'),
 ]
